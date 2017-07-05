94c87456fc3aaae0280ba0290c784b4c5c21e3fd
Nir Izraeli <nirizr@gmail.com>
2017-07-05 16:17:50 -0700

    Initial commit
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

---
+++ main.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import re
+import shutil
+import traceback
+
+import argh
+import win32api
+import vss
+
+
+def get_drives():
+  return [drive.split(":")[0] for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]]
+
+
+def find_files(drive, regex):
+  path = "{}:\\".format(drive)
+  for root, _, files in os.walk(path):
+    for f in files:
+      result = regex.search(f)
+      if result:
+        yield os.path.join(root, f)
+
+
+def restore_drive(drive, vss, regex, quiet=False):
+  print("Restoring drive {}".format(drive))
+  successful = 0
+  total = 0
+  for encrypted_file in find_files(drive, regex):
+    total += 1
+    original_file = os.path.splitext(encrypted_file)[0]
+    try:
+      vss_file = vss.shadow_path(original_file)
+    except:
+      print("Original file: {}".format(original_file))
+      print("ERROR: failed converting to VSS name")
+      traceback.print_exc(file=sys.stdout)
+      continue
+    restored_file = os.path.splitext(original_file)[0] + ".restored" + os.path.splitext(original_file)[1]
+    try:
+      with open(vss_file, 'rb') as vss_fh:
+        with open(restored_file, 'wb') as restored_fh:
+          shutil.copyfileobj(vss_fh, restored_fh)
+          successful += 1
+          print("Successfully restored file at {}".format(restored_file))
+    except:
+      if not quiet:
+        print("Restoring file {}".format(encrypted_file))
+        print("Original file: {}".format(original_file))
+        print("VSS file: {}".format(vss_file))
+        print("Restored file: {}".format(restored_file))
+        print("ERROR: failed restoring above file!")
+        traceback.print_exc(file=sys.stdout)
+  print("Finished restoring attempt. Found {} files, restored {}".format(total, successful))
+
+
+def main(extension="moments2900", regex=None, quiet=False):
+  drives = get_drives()
+  print("Found following drives: {}".format(drives))
+
+  if not regex:
+    print("Looking for extension: {}".format(extension))
+    regex = "\.{}$".format(extension)
+
+  regex = re.compile(regex)
+
+  vss = vss.ShadowCopy()
+  for drive in drives:
+    handle_drive(drive, vss, regex, quiet)
+
+
+if __name__ == "__main__":
+  argh.dispatch_command(main)
---
+++ vss.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import win32com.client
+
+class ShadowCopy:
+    def __init__(self):
+        """
+        Creates shadow copies for each local drive in the set drive_letters.
+        """
+        self.__drive_letters = set()
+        self.__shadow_ids = {}
+
+    def shadow_path(self, path):
+        '''
+        Takes a regular file system path and transforms it into an
+        equivalent path in a shadow copy
+        '''
+        # TODO: validate path starts with a drive letter
+        drive_letter = path[0]
+        for shadow_path in self.__shadow_paths:
+            new_path = path.replace(drive_letter + u':',
+                                    shadow_path,
+                                    1)
+            try:
+                with open(new_path, 'rb') as fh:
+                    return new_path
+            except:
+                pass
+        raise Exception("Problem processing path: {0}".format(path))
+
+    def __vss_list(self):
+        wcd=win32com.client.Dispatch("WbemScripting.SWbemLocator")
+        wmi=wcd.ConnectServer(".","root\cimv2")
+        # TODO: sort by time so first SS path will be the most recent and so on...
+        obj=wmi.ExecQuery("SELECT * FROM Win32_ShadowCopy")
+        return [unicode(o.DeviceObject) for o in obj]
